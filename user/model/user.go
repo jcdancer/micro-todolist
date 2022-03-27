1e4922c0ed4edf445bbe388b985a3fd26c44e836
jiangchun <jiangchun.jc@bytedance.com>
2022-03-27 17:26:22 +0800

    feat:add user proto

---
+++ user/model/user.go
@@ -0,0 +1,35 @@
+package model
+
+import (
+	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	gorm.Model
+	UserName       string `gorm:"unique"`
+	PasswordDigest string
+}
+
+const (
+	PassWordCost = 12 // 密码加密难度
+)
+
+// SetPassword
+// @Description: 加密密码
+// @receiver user
+// @param password
+// @return error
+func (user *User) SetPassword(password string) error {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), PassWordCost)
+	if err != nil {
+		return err
+	}
+	user.PasswordDigest = string(bytes)
+	return nil
+}
+
+func (user *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(user.PasswordDigest), []byte(password))
+	return err == nil
+}
